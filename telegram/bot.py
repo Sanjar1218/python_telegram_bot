60c9b300c7bb1feb5eb606614727e26eb3b4c447
sanjarbek <sanjarsaidov173@gamil.com>
2022-08-20 09:59:25 +0500

    doc added

--- telegram/bot.py
+++ telegram/bot.py
@@ -10,11 +10,26 @@ class Bot:
             self.base_url = base_url+token
 
     def getMe(self):
+        """A simple method for testing your bot's auth token.
+        Returns:
+          A telegram.User instance representing that bot if the
+          credentials are valid, None otherwise.
+        """
         url = f'{self.base_url}/getMe'
         r = requests.get(url)
         return User.newJsonFromDict(r.json())
     
     def sendMessage(self, chat_id, text):
+        """Use this method to send text messages.
+        Args:
+          chat_id:
+            Unique identifier for the message recipient — telegram.User or
+            telegram.GroupChat id.
+          text:
+            Text of the message to be sent.
+        Returns:
+          A telegram.Message instance representing the message posted.
+        """
         url = f'{self.base_url}/sendMessage'
 
         payload = {
@@ -33,6 +48,17 @@ class Bot:
         return Update.newFromJsonDict(r.json()['result'][-1])
     
     def _requestUrl(self, url, method, data=None):
+        """Request an URL.
+        Args:
+          url:
+            The web location we want to retrieve.
+          method:
+            Either POST or GET.
+          data:
+            A dict of (str, unicode) key/value pairs.
+        Returns:
+          A JSON object.
+        """
         if method == 'post':
             try:
                 return requests.post(url, data=data)
